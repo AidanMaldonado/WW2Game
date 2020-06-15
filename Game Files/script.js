2c96ab600655fc372138c0e4e134c47eaee232f0
Aidan Maldonado <aidanm1198@gmail.com>
2020-06-15 00:48:30 -0400

    Added basic functionality
    Added css styling
    Added Layout for troops
    Created starting constructors

--- Game Files/script.js
+++ Game Files/script.js
@@ -1,11 +1,54 @@
-let operationNamesArray = ["Tiger"];
+let operationNamesArray = [
+  "Tiger Strike",
+  "Blue Sun",
+  "Hidden Lilly",
+  "Urban Garden",
+  "Evening Light",
+  "Trident",
+  "Sunken Coast",
+  "Mongoose",
+  "Urgent Fury",
+  "Teardrop",
+  "Broken Glass",
+  "Capital Storm",
+];
 
-function operationGenerator() {
-  var arrayLength = operationNamesArray.length;
-  var randomNum = Math.random() * arrayLength;
-  console.log(randomNum);
+//Game vars
+let teamOneTickets = 1000;
+let teamTwoTickets = 1000;
+let teamOneCash = 50000;
+let teamTwoCash = 50000;
+//DOM vars
+let teamOneTicketTitle = document.getElementById("teamOneTickets");
+let teamTwoTicketTitle = document.getElementById("teamTwoTickets");
+let teamOneCashTitle = document.getElementById("teamOneCash");
+let teamTwoCashTitle = document.getElementById("teamTwoCash");
 
-  document.getElementById("operationName").innerHTML = "Operation Pain";
+//Constructor for the unit type
+function UnitType(unit, atk, cost) {
+  this.unitName = unit;
+  this.attackDmg = atk;
+  this.costOfUnit = cost;
 }
 
-operationGenerator();
+const footSoldier = new UnitType("Foot Soldier", 1, 100);
+
+//Generates the name of the operation at game start
+function operationGenerator() {
+  var arrayLength = operationNamesArray.length;
+  var randomNum = Math.floor(Math.random() * arrayLength + 1);
+  var opName = operationNamesArray[randomNum - 1];
+
+  document.getElementById("operationName").innerHTML = "Operation " + opName;
+}
+
+//Function is employed at game start
+function gameStart() {
+  operationGenerator();
+  teamTwoTicketTitle.innerHTML = teamTwoTickets;
+  teamOneTicketTitle.innerHTML = teamOneTickets;
+  teamOneCashTitle.innerHTML = "$" + teamOneCash;
+  teamTwoCashTitle.innerHTML = "$" + teamTwoCash;
+}
+
+gameStart();
